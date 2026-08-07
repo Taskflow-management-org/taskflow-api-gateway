c82478c5303b49f40d058bbf549d96d253b08c79
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-08-07 19:57:18 +0530

    test(config): setup test properties for application context validation
    Add @SpringBootTest configuration
    Inject JWT secret key using test properties
    Verify Spring application context initialization

--- src/test/java/com/thamindu/task_manager/TaskManagerApplicationTests.java
+++ src/test/java/com/thamindu/task_manager/TaskManagerApplicationTests.java
@@ -2,8 +2,12 @@ package com.thamindu.task_manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"jwt.secret-key=404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970"
+})
 class TaskManagerApplicationTests {
 
 	@Test

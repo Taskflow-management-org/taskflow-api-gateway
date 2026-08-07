116bc26eaf80585db75b4bc029ead90c63af3fa7
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-08-07 19:54:18 +0530

    feat: add JWT filter for gateway authentication

--- src/main/java/com/thamindu/task_manager/filter/JwtMvcFilter.java
+++ src/main/java/com/thamindu/task_manager/filter/JwtMvcFilter.java
@@ -1,4 +1,115 @@
 package com.thamindu.task_manager.filter;
 
-public class JwtMvcFilter {
+import com.thamindu.task_manager.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+import java.util.*;
+
+@Component
+@RequiredArgsConstructor
+public class JwtMvcFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request){
+        String path = request.getRequestURI();
+
+        return path.startsWith("/api/v1/auth/") ||
+                path.startsWith("/visitor/");
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws IOException, ServletException {
+
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.getWriter().write("Missing or Invalid Authorization Header");
+            return;
+        }
+
+        String token = authHeader.substring(7);
+
+        if (!jwtUtil.isTokenValid(token)){
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.getWriter().write("Invalid or Expired JWT Token");
+            return;
+        }
+
+       Claims claims = jwtUtil.extractAllClaims(token);
+       String userId = claims.getSubject();
+       String email = claims.get("email", String.class);
+
+       HeaderMapRequestWrapper requestWrapper = new HeaderMapRequestWrapper(request);
+       requestWrapper.addHeader("X-User-Id", userId != null? userId : "");
+       requestWrapper.addHeader("X-User-Email", email != null? email : "");
+
+       filterChain.doFilter(request,response);
+
+    }
 }
+
+
+class HeaderMapRequestWrapper extends HttpServletRequestWrapper {
+    private final Map<String, String> customHeaders = new HashMap<>();
+
+    public HeaderMapRequestWrapper(HttpServletRequest request) {
+        super(request);
+    }
+
+    public void addHeader(String name, String value){
+        this.customHeaders.put(name,value);
+    }
+
+
+    @Override
+    public String getHeader(String name) {
+        String headerValue = customHeaders.get(name);
+
+        if (headerValue != null){
+            return headerValue;
+        }
+
+        return super.getHeader(name);
+
+    }
+
+    @Override
+    public Enumeration<String> getHeaderNames() {
+        List<String> names = Collections.list(super.getHeaderNames());
+        names.addAll(customHeaders.keySet());
+        return Collections.enumeration(names);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

9f519d927ad9aaaafe4ff6909cf2a620b7e5a3dc
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-08-12 19:07:46 +0530

    fix: update gateway routing targets from localhost to docker container names

--- src/main/java/com/thamindu/task_manager/config/GatewayConfig.java
+++ src/main/java/com/thamindu/task_manager/config/GatewayConfig.java
@@ -15,14 +15,14 @@ public class GatewayConfig {
     @Bean
     public RouterFunction<ServerResponse> userServiceRoute(){
       return route("user-service")
-              .route(path("/api/v1/auth/**"), http("http://localhost:8081"))
+              .route(path("/api/v1/auth/**"), http("http://user-service:8081"))
               .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> taskServiceRoute(){
         return route("task-service")
-                .route(path("/api/v1/task/**"), http("http://localhost:8082"))
+                .route(path("/api/v1/task/**"), http("http://task-service:8082"))
                 .build();
     }
 
